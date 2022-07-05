dc924a5b2b984bd420341144d3e131d258122cb0
DavidFulton1 <fultondavids@gmail.com>
2022-07-05 11:53:48 -0500

    second commit

---
+++ Methods excercises/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsexcerecise;
+
+internal class Class1
+{
+    public static int Add(int num1, int num2)
+    { 
+        return num1 + num2;
+    }
+    public static int Multiply(int num3, int num4)
+    { 
+        
+        return num3 * num4;
+    }
+    public static int Divide(int num5, int num6)
+    {
+        return num5 / num6;
+    }
+    public static int Subtract(int num7,int num8)
+    { return num7 - num8; }
+}
+
+
+
+
+
+
+
+
