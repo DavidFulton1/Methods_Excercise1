137b76d82d84460afaa224a0fd2e96cdb4e80146
DavidFulton1 <fultondavids@gmail.com>
2022-07-05 10:25:19 -0500

    first commit

---
+++ method excercise.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hi, Im David! Whats your name?");
+string name=Console.ReadLine();
+Console.WriteLine("What is your favorite color");
+string color = Console.ReadLine();
+Console.WriteLine("Thats awesome!");
+Console.WriteLine("What is your favorite music band?");
+string music =Console.ReadLine();
+Console.WriteLine("groovey dawg");
+Console.WriteLine($"Well {name}, in that case I know what to get you for your birthday. Its going to be a {color} shirt from {music}'s last album.");
